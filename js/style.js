476092a44c2858520baccdc5dc424228ddf94bf7
kristamarette <55501269+kristamarette@users.noreply.github.com>
2019-12-11 14:39:07 -0600

    updated css on Recipes

--- js/style.js
+++ js/style.js
@@ -1,39 +1,48 @@
+//<---------Chevron -------->
+
+//  $("chevron").hover(function(){
+//       $("chevron").slideDown();
+//     });
+
+// <----------Shaker Motion---------->
+
 var tl = anime.timeline({
     easing: 'easeOutExpo',
-    duration: 750
+    duration: 100,
 });
     tl.add({
-        targets: 'section#section1 div',
-        width: '100%',
-        backgroundColor: 'rgb(197,197,255)',
+        targets: 'img#shaker',
+        width: '25%',
+        // backgroundColor: 'rgb(197,197,255)',
         delay: anime.stagger(100),
     })
     tl.add({
-        targets: 'section#section1 div',
-        width: '90%',
-        backgroundColor: 'rgb(235,235,255)',
-        delay: anime.stagger(100),
+        targets: 'img#shaker',
+        width: '20%',
+        // backgroundColor: 'rgb(235,235,255)',
+        delay: anime.stagger(4000),
     })
 
     tl.add({
-        targets: 'h1',
+        targets: 'img#shaker',
         top: '20%',
         opacity: 1,
-        duration: 4000,
+        duration: 500,
     }, '-= 1600')
     
     let rotateMe = anime ({
-        targets: 'section#section1',
-        scaleY:'2',
-        scaleX: '2',
-        translateX: '40%',
+        targets: 'img#shaker',
+        scaleY:'1',
+        scaleX: '1',
+        translateX: '20%',
+        // rotate: '-45deg',
         rotate: '45deg',
-        duration: 5000,
+        duration: 2000,
         autoplay: true,
     })
-    document.querySelector('h1')
+
+    document.querySelector('img#shaker')
     .addEventListener('mouseover',()=>
     {
         rotateMe.play();
-        
-    });
+ });
